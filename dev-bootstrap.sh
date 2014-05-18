976996cb9588e073fb5a2a9036534e1b7d6f3ddb
root <root@workflow-test-1.(none)>
2014-05-18 18:58:45 +0000

    Remove extra comments from bootstrap script

--- dev-bootstrap.sh
+++ dev-bootstrap.sh
@@ -1,17 +1,10 @@
 # CONFIGURATION
 OS_NAME='precise'
 
-# Create new droplet
-
-# Run the rest of this script on new server
-
 # Install Git
-apt-get -y install git
-
-# Upgrade Git
-git clone git://git.kernel.org/pub/scm/git/git.git
-
-# Install Ruby or RVM?
+add-apt-repository ppa:git-core/ppa
+apt-get update
+apt-get install git
 
 # Install Puppet
 wget https://apt.puppetlabs.com/puppetlabs-release-$OS_NAME.deb

a12cfa03d163e657cf2a5b9b7c4c794ed2a07da3
root <root@workflow-test-1.(none)>
2014-05-18 18:50:11 +0000

    Install Puppet

--- dev-bootstrap.sh
+++ dev-bootstrap.sh
@@ -1,5 +1,20 @@
+# CONFIGURATION
+OS_NAME='precise'
+
+# Create new droplet
+
+# Run the rest of this script on new server
+
 # Install Git
-apt-get -q -y install git
+apt-get -y install git
 
 # Upgrade Git
 git clone git://git.kernel.org/pub/scm/git/git.git
+
+# Install Ruby or RVM?
+
+# Install Puppet
+wget https://apt.puppetlabs.com/puppetlabs-release-$OS_NAME.deb
+sudo dpkg -i puppetlabs-release-$OS_NAME.deb
+sudo apt-get update
+sudo apt-get -y install puppet
